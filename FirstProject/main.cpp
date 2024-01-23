b7c1e1330b4fa41d3a08b51f97ce73a04f6c0d41
Christian Pazmiño <cris19957@live.com>
2024-01-23 01:14:56 -0500

    multiple cubes support

--- FirstProject/main.cpp
+++ FirstProject/main.cpp
@@ -115,7 +115,8 @@ void init(GLFWwindow* window) {
 
 void display(GLFWwindow* window, double currentTime) { // Default Program
 	glClear(GL_DEPTH_BUFFER_BIT);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT); //clearing the depth buffer each time through 
+															  //display() to ensure correct hidden surface removal
 	glm::mat4 tMat, rMat;
 	glUseProgram(renderingProgram);
 	// get the uniform variables for the MV and projection matrices
@@ -126,29 +127,35 @@ void display(GLFWwindow* window, double currentTime) { // Default Program
 	aspect = (float)width / (float)height;
 	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
 	// build view matrix, model matrix, and model-view matrix
-	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-	tMat = glm::translate(glm::mat4(1.0f),
-												glm::vec3(sin(0.35f * currentTime) * 2.0f,
-																	cos(0.52f * currentTime) * 2.0f, 
-																	sin(0.7f * currentTime) * 2.0f));
-	rMat = glm::rotate(glm::mat4(1.0f), 1.75f * (float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
-	rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
-	rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
-	mMat = tMat * rMat;
-	mvMat = vMat * mMat;
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -3.f * cameraZ));
+	float tf;
+	for (int i = 0; i < 24; i++) {
+		tf = currentTime + i; // tf == "time factor", declared as type float
+		// use current time to compute different translations in x, y, and z
+		tMat = glm::translate(glm::mat4(1.0f), 
+													glm::vec3(
+													sin(.35f * tf) * 8.0f, 
+													cos(.52f * tf) * 8.0f,
+													sin(.70f * tf) * 8.0f));
+		rMat = glm::rotate(glm::mat4(1.0f), 1.75f * (float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
+		rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
+		rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
+		mMat = tMat * rMat;
+		mvMat = vMat * mMat;
 
-	// copy perspective and MV matrices to corresponding uniform variables
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-	// associate VBO with the corresponding vertex attribute in the vertex shader
-	// glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // already binded??
-	// is used to specify the data format of individual vertex attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-	// adjust OpenGL settings and draw model
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+		// copy perspective and MV matrices to corresponding uniform variables
+		glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+		// associate VBO with the corresponding vertex attribute in the vertex shader
+		// glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // already binded??
+		// is used to specify the data format of individual vertex attributes
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(0);
+		// adjust OpenGL settings and draw model
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 }
 
 int main(void) {
