1867f6ca3e5d00c0fe18a8686adbe65ad47b19cb
Christian Pazmiño <cris19957@live.com>
2024-01-24 00:42:04 -0500

    placing cube and pyramid

--- FirstProject/main.cpp
+++ FirstProject/main.cpp
@@ -17,18 +17,20 @@ using namespace std;
 
 float cameraX, cameraY, cameraZ;
 float cubeLocX, cubeLocY, cubeLocZ;
+float pyrLocX, pyrLocY, pyrLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // Allocate variables used in display() function, so that they won’t need to be allocated during rendering
-GLuint vLoc, projLoc;
+GLuint mvLoc, projLoc;
 int width, height;
 float aspect, tfLoc;
 glm::mat4 pMat, vMat, mMat, mvMat;
 
-void setupVertices(void) { // 36 vertices, 12 triangles, 6 faces, make 2x2x2 cube placed at origin
-	float vertexPositions[108] = {
+void setupVertices(void) { 
+	// cube with 36 vertices, 12 triangles, 6 faces, make 2x2x2 cube placed at origin
+	float cubePositions[108] = {
 		// Face 1
 
 	 -1.0f,	 1.0f, -1.0f,
@@ -89,6 +91,38 @@ void setupVertices(void) { // 36 vertices, 12 triangles, 6 faces, make 2x2x2 cub
 	 -1.0f,  1.0f,  1.0f,
 	 -1.0f,  1.0f, -1.0f
 	};
+	// pyramid with 18 vertices, comprising 6 triangles (four sides, and two on the bottom)
+	float pyramidPositions[54] = { 
+		// front face
+		-1.0f, -1.0f, 1.0f, 
+		 1.0f, -1.0f, 1.0f, 
+		 0.0f, 1.0f, 0.0f, 
+		 
+		// right face
+		 1.0f, -1.0f, 1.0f, 
+		 1.0f, -1.0f, -1.0f, 
+		 0.0f, 1.0f, 0.0f, 
+
+		// back face
+		 1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, -1.0f, 
+		 0.0f, 1.0f, 0.0f,
+
+		// left face
+	  -1.0f, -1.0f, -1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+		 0.0f, 1.0f, 0.0f, 
+
+		 // base – left front
+	  -1.0f, -1.0f, -1.0f, 
+		 1.0f, -1.0f, 1.0f, 
+		-1.0f, -1.0f, 1.0f, 
+		
+		 // base – right back
+	   1.0f, -1.0f, 1.0f, 
+		-1.0f, -1.0f, -1.0f, 
+		 1.0f, -1.0f, -1.0f 
+	};
 	std::cout << "Initializing VAOs and VBOs..." << std::endl;
 	// Initialize Vertex Array Object (VAO) and Vertex Buffer Objects (VBOs)
 	glGenVertexArrays(numVAOs, vao);			 // Generate numVAOs = 1 VAO and store its ID all along vao[] array
@@ -99,11 +133,20 @@ void setupVertices(void) { // 36 vertices, 12 triangles, 6 faces, make 2x2x2 cub
 	// Generate and bind Vertex Buffer Object (VBO)
 	glGenBuffers(numVBOs, vbo);						 // Generate numVBOs = 2 VBOs and store their IDs all along vbo[] array
 																				 // So at this point, vbo[0] and vbo[1] already contain an ID value
+	
+	//VBO 1
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // Select the vbo[0] ID as the current GL_ARRAY_BUFFER, we have 2 in this case
 																				 // 1. vao[0] SELECTED AND vbo[0] SELECTED by their ID
 	// So from now on vao[0] and vbo[0] is the current buffer where our instructions are going to be applied
 	// Upload vertex data to the selected VBO. Note that HERE is where we define the size of the buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubePositions), cubePositions, GL_STATIC_DRAW);
+	
+	//VBO 2
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); // Select the vbo[0] ID as the current GL_ARRAY_BUFFER, we have 2 in this case
+																				 // 1. vao[0] SELECTED AND vbo[1] SELECTED by their ID
+	// So from now on vao[0] and vbo[0] is the current buffer where our instructions are going to be applied
+	// Upload vertex data to the selected VBO. Note that HERE is where we define the size of the buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pyramidPositions), pyramidPositions, GL_STATIC_DRAW);
 }
 
 void init(GLFWwindow* window) {
@@ -117,32 +160,42 @@ void display(GLFWwindow* window, double currentTime) { // Default Program
 	glClear(GL_DEPTH_BUFFER_BIT);
 	glClear(GL_COLOR_BUFFER_BIT); //clearing the depth buffer each time through 
 																//display() to ensure correct hidden surface removal
-;
 	glUseProgram(renderingProgram);
-	glfwGetFramebufferSize(window, &width, &height);
 
-	//computations that build(and transform) mMat have been moved to the vertex shader.
-	// there is no longer any need to build an MV matrix in the C++ application.
-	vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+	// get the uniform variables for the MV and projection matrices
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-	tfLoc = glGetUniformLocation(renderingProgram, "tf"); // (the shader needs that too)
-	aspect = (float)width / (float)height;
 
-	// build perspective and view matrix
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
 	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
-	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -20.0*cameraZ));
-	float timeFactor = ((float)currentTime); // uniform for the time factor
+
+	// the view matrix is computed once and used for both objects
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+
+	// draw the cube (use buffer #0)
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	mvMat = vMat * mMat;
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat)); // shader needs the V matrix
-	glUniform1f(tfLoc, (float)timeFactor);
-	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
-	// adjust OpenGL settings and draw model
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
-	glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100000);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
+	// draw the pyramid (use buffer #1)
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(pyrLocX, pyrLocY, pyrLocZ));
+	mvMat = vMat * mMat;
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 18);
 }
 
 int main(void) {
